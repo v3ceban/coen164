160b70ce7c6effc375c8f5117d46fb6a03ef0fca
Vladimir Ceban <v3ceban@proton.me>
2024-04-04 08:35:25 -0700

    init commit

---
+++ first.rb
@@ -0,0 +1,22 @@
+a = 5
+b = 6
+
+puts a + b
+
+def my_method
+  a = 'i love you'
+  puts a
+end
+
+my_method
+
+puts a.class
+
+puts a**2
+
+puts a.odd?
+puts a.even?
+puts b.odd?
+puts b.even?
+
+5.times { puts 'hello' }
