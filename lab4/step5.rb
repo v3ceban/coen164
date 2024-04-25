abdcb7344ffe7aff6314f82a64ecf848abb3ed0d
Vladimir Ceban <v3ceban@proton.me>
2024-04-25 12:23:11 -0700

    lab4

--- lab4/step5.rb
+++ lab4/step5.rb
@@ -11,23 +11,34 @@ cgi = CGI.new('html5')
 # Start session
 session = CGI::Session.new(cgi, 'session_key' => 'mysession')
 
-@message = if session['visit']
-             "You have visited: #{session['visit']} times"
-           else
-             "You haven't been here recently"
-           end
+message = if session['visit']
+            "You have visited: #{session['visit']} times"
+          else
+            "You haven't been here recently"
+          end
 
 count = (session['visit'] || 0).to_i
 count += 1
+
 session['visit'] = count
+
 session.close
 
-@name = cgi.params['name'].first || 'name is empty'
-@reason = cgi.params['reason'].first || 'reason is empty'
+p1 = if cgi.params['name'] == ''
+       cgi.params['name']
+     else
+       'name is empty'
+     end
 
-# this template can technically be moved to a different file (just like in step1)
+p2 = if !cgi.params['reason'].empty?
+       cgi.params['reason']
+     else
+       'reason is empty'
+     end
+
+# this template can technically be moved to a different file
 # but it feels weird to have it there as the script itself is in cgi-bin folder
-# which I guess should contain cgi code only. idk
+# that I guess should contain cgi code only. idk
 template = ERB.new(<<~ERB)
   <!DOCTYPE html>
   <html lang="en">
@@ -37,9 +48,9 @@ template = ERB.new(<<~ERB)
   </head>
   <body>
     <h1>Your submit from the form:</h1>
-    <p>Name: <%= @name %></p>
-    <p>Reason: <%= @reason %></p>
-    <p><%= @message %></p>
+    <p>Name: <%= p1 %></p>
+    <p>Reason: <%= p2 %></p>
+    <p><%= message %></p>
   </body>
   </html>
 ERB
